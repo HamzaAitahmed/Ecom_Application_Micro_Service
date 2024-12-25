0710feb2c695dc1338a54fbb1e2fb9687548e776
HamzaAitahmed <hamzaaitahmed03@gmail.com>
2024-12-25 22:02:23 +0100

    Ajouter Vault Parametres Automatiquement depuis consul avant demarrer Vault-service

--- consul-service/src/main/java/ma/emsi/consulservice/ConsulServiceApplication.java
+++ consul-service/src/main/java/ma/emsi/consulservice/ConsulServiceApplication.java
@@ -1,10 +1,15 @@
 package ma.emsi.consulservice;
 
 import ma.emsi.consulservice.Controller.ConsulConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultTemplate;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,5 +20,12 @@ public class ConsulServiceApplication {
         SpringApplication.run(ConsulServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(VaultTemplate vaultTemplate) {
+        return args -> {
+            vaultTemplate.opsForVersionedKeyValue("secret")
+                    .put("vault-service", Map.of("user.username", "Hamza AitAhmed", "user.password", "P@ssw0rd") );
 
+        };
+    }
 }
